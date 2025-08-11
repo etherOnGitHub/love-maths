3c2f5b851e4f0035c9850e9e0041efd249124a9b
Callum R <cal.richards@hotmail.com>
2025-08-11 12:37:05 +0100

    add: basic functions
    add: functions in JS to outline basic operations and game logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
-console.log("Script loaded successfully!");
+function runGame() {
+    // Logic to start the game
+}
+
+function checkAnswer() {
+    // Logic to check the user's answer
+}
+
+function calculateCorrectAnswer() {
+    // Logic to calculate the correct answer based on the current question
+}
+
+function incrementScore() {
+    // Logic to increment the score
+}
+
+function incrementWrongAnswer() {
+    // Logic to increment the wrong answer count
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+    // Logic to display an addition question
+}
+
+function displaySubtractQuestion(operand1, operand2) {
+    // Logic to display a subtraction question
+}
+
+function displayMultiplyQuestion(operand1, operand2) {
+    // Logic to display a multiplication question
+}
+function displayDivisionQuestion(operand1, operand2) {
+    // Logic to display a division question
+}
