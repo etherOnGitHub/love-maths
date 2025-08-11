3a3c5b9b7fe814c014108bdf4befb59339b3a218
Callum R <cal.richards@hotmail.com>
2025-08-11 12:47:36 +0100

    add: DOM load check and simple event listeners
    DOM load checker executes and buttons are listened to and if clicked throws an alert to confirm operation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+//DOM load checker
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.querySelectorAll("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit")
+        } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })    
+    }
+})
+    // Initialize the game when the DOM is fully loaded
+
 function runGame() {
     // Logic to start the game
 }
@@ -32,3 +49,4 @@ function displayMultiplyQuestion(operand1, operand2) {
 function displayDivisionQuestion(operand1, operand2) {
     // Logic to display a division question
 }
+
